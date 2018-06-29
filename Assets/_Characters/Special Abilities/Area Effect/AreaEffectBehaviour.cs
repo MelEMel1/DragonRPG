0bed2b94159ffdfb42a7eddb110a6091a89b2e82
MelEMel <melissa.coulton@gmail.com>
2018-06-29 15:53:20 -0300

    added area effect special ability

---
+++ Assets/_Characters/Special Abilities/Area Effect/AreaEffectBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+using RPG.Core;
+
+public class AreaEffectBehaviour : MonoBehaviour, ISpecialAbility {
+
+    AreaEffectConfig config;
+
+    public void SetConfig(AreaEffectConfig configToSet)
+    {
+        this.config = configToSet;
+    }
+
+	// Use this for initialization
+	void Start () {
+		print ("Area Effect Behaviour attached to " + gameObject);
+	}
+	
+    public void Use(AbilityUseParams useParams)
+    {
+        print ("Area Effect used by " + gameObject);
+        // Static sphere cast for targets
+        RaycastHit[] hits = Physics.SphereCastAll(
+                transform.position,
+                config.GetRadius(),
+                Vector3.up,
+                config.GetRadius()
+            );
+
+        foreach(RaycastHit hit in hits)
+        {
+            var damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+            if(damageable != null)
+            {
+                float damageToDeal = useParams.baseDamage + config.GetDamageToEachTarget();
+                damageable.TakeDamage(damageToDeal);
+            }
+        }
+        // for each hit
+        // if damageable
+            // deal damage to target + player base damage
+    }
+}
